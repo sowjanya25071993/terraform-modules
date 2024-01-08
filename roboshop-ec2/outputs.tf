5d96a0bdcbe30d10d0a6774417c512d3c71b17ef
sowji <jujjurisowjanya@gmail.com>
2024-01-08 18:30:13 +0530

    new

---
+++ roboshop-ec2/outputs.tf
@@ -0,0 +1,9 @@
+output "public-ip" {
+    value = module.roboshop-ec2.public_ip
+}
+output "private-ip" {
+    value = module.roboshop-ec2.private_ip
+}
+output "id" {
+    value = module.roboshop-ec2.id
+}
---
+++ roboshop-ec2/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+    default "t3.medium"
+
+}
+variable "tags" {
+    default = {
+        Name = "roboshop"
+        env = "dev"
+        terraform = "true"
+    }
+}
